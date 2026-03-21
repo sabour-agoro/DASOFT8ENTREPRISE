23ff2765419dae0512615d286daa317b6c4c1649
sabour-agoro <abdoulsabouragoro@gmail.com>
2026-03-21 03:04:19 +0100

    .

--- src/Pages/CardNav.jsx
+++ src/Pages/CardNav.jsx
@@ -6,8 +6,9 @@ import Projet from './Projet';
 
 const ArrowIcon = () => (
   <svg stroke="currentColor" fill="none" strokeWidth="2" viewBox="0 0 24 24" strokeLinecap="round" strokeLinejoin="round" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
-    <line x1="7" y1="17" x2="17" y2="7"></line>
     <polyline points="7 7 17 7 17 17"></polyline>
+    <line x1="7" y1="17" x2="17" y2="7"></line>
+    
   </svg>
 );
 
